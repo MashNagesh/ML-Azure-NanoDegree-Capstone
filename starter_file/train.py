e9d57ec8c98e391d1f7968a3808028ee73f81400
Mahalakshmi Nageswaran <mashrajiv@gmail.com>
2021-07-28 15:28:38 +0530

    Push
    Publish to Capstone Project

---
+++ starter_file/train.py
@@ -0,0 +1,50 @@
+from sklearn.linear_model import LogisticRegression
+import argparse
+import os
+import numpy as np
+from sklearn.metrics import mean_squared_error
+import joblib
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import OneHotEncoder
+import pandas as pd
+from azureml.core.run import Run
+from azureml.data.dataset_factory import TabularDatasetFactory
+from azureml.core import  Dataset
+
+#Data Location
+train_properties_file = './data/train.csv'
+test_properties_file = './data/test.csv'
+
+#Read Train and Test Data
+train = pd.read_csv(train_properties_file)
+test = pd.read_csv(test_properties_file)
+
+# TODO: Split data into train and test sets.
+y = train.label
+train_mod = train.drop(columns=['label'],axis=1)
+x_train,x_test,y_train,y_test = train_test_split(train_mod,y, test_size=0.75, random_state=42)
+
+run = Run.get_context()
+
+def main():
+    # Add arguments to script
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('--C', type=float, default=1.0, help="Inverse of regularization strength. Smaller values cause stronger regularization")
+    parser.add_argument('--max_iter', type=int, default='100', help="Maximum number of iterations to converge")
+
+    args = parser.parse_args()
+
+    run.log("Regularization Strength:", np.float(args.C))
+    run.log("Max iterations:", np.int(args.max_iter))
+
+    model = LogisticRegression(C=args.C, max_iter=args.max_iter).fit(x_train, y_train)
+    
+    os.makedirs('outputs', exist_ok=True)
+    joblib.dump(model, 'outputs/model.joblib')
+
+    accuracy = model.score(x_test, y_test)
+    run.log("Accuracy", np.float(accuracy))
+
+if __name__ == '__main__':
+    main()
